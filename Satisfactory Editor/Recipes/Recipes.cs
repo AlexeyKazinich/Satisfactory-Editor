36d2ea97d2025c15d4f8cca2ee97482c6621c742
AlexeyKazinich <alexey.kazinich@gmail.com>
2021-08-29 18:53:14 -0400

    implemented the Recipes abstract class
    Added the recipes abstract class which will be used for all recipes later on. Also added some documentation for the abstract class

---
+++ Satisfactory Editor/Recipes/Recipes.cs
@@ -0,0 +1,24 @@
+﻿using Satisfactory_Editor.Items;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Satisfactory_Editor.Recipes
+{
+    public abstract class Recipes
+    {
+        /* Precondition: the value "amount" needs to be smaller or equal to the available items in the Dictionary
+         * This method Expects: amount of buildings, the Dictionary with all the items.
+         * PostCondition: returns the Dictionary with all the items updated
+         */
+        public abstract IDictionary<string, Item> addItem(int amount, IDictionary<string, Item> Dictionary);
+
+        /* Precondition: the value "amount" needs to be smaller or equal to the available items in the Dictionary
+         * This method Expects: amount of buildings, the Dictionary with all the items.
+         * PostCondition: returns the Dictionary with all the items updated
+         */
+        public abstract IDictionary<string, Item> removeItem(int amount, IDictionary<string, Item> Dictionary);
+    }
+}
--- Satisfactory Editor/Recipes/Smelter/IronIngotRecipe.cs
+++ Satisfactory Editor/Recipes/Smelter/IronIngotRecipe.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Satisfactory_Editor.Items;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,62 @@ using System.Threading.Tasks;
 
 namespace Satisfactory_Editor.Recipes.Smelter
 {
-    public class IronIngotRecipe
+    public class IronIngotRecipe : Recipes
     {
+        public IDictionary<string, int> itemUsing;
+        public IDictionary<string,int> itemMaking; 
+
+        public IronIngotRecipe()
+        {
+            itemUsing = new Dictionary<string, int>();
+            itemMaking = new Dictionary<string, int>();
+            addItemUsingAndMaking();
+        }
+
+        private void addItemUsingAndMaking()
+        {
+            //add to dictionary the items its using
+            itemUsing.Add("rawIron", 30);
+
+            //add to dictionary the items its making
+            itemMaking.Add("ironIngot", 30);
+        }
+
+        public override IDictionary<string, Item> addItem(int amount, IDictionary<string, Item> Dictionary)
+        {
+
+            foreach (KeyValuePair<string,int> entry in itemUsing)
+            {
+                Dictionary[$"{entry.Key}"].amountUsing += ((itemUsing[$"{entry.Key}"])*amount);
+            }
+
+
+            foreach (KeyValuePair<string,int> entry in itemMaking)
+            {
+                Dictionary[$"{entry.Key}"].amountCreating += ((itemMaking[$"{entry.Key}"])*amount);
+            }
+
+            return Dictionary;
+        }
+
+
+
+        public override IDictionary<string, Item> removeItem(int amount, IDictionary<string, Item> Dictionary)
+        {
+            //removes items Using from Dictionary
+            foreach (KeyValuePair<string, int> entry in itemUsing)
+            {
+                Dictionary[$"{entry.Key}"].amountUsing -= ((itemUsing[$"{entry.Key}"]) * amount);
+            }
+
+            //removes items Making from Dictionary
+            foreach (KeyValuePair<string, int> entry in itemMaking)
+            {
+                Dictionary[$"{entry.Key}"].amountCreating -= ((itemMaking[$"{entry.Key}"]) * amount);
+            }
+
+            return Dictionary;
+
+        }
     }
 }
